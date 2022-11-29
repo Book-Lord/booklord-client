974bc7a137b6f28f1160b935641a3d1516ebaea5
Iskren Alexandrov <iskren.alexandrov@gmail.com>
2022-11-29 08:34:05 +0200

    Create side bar

---
+++ components/BookPreview.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="box-border inline-block mt-4 mr-4 h-72 w-48 p-4 border-4">
+        <p class="center">Harry Potter: The TUES thesis</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BookPreview"
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <div>
+      <side-bar />
+      <div class="ml-80">
+         <book-preview />
+         <book-preview />
+         <book-preview />
+         
+      </div>
+   </div>
 </template>
 
 <script>
+import BookPreview from '~/components/BookPreview.vue';
+
 export default {
-  name: 'IndexPage',
+    name: "HomePage",
+    components: { BookPreview }
 }
 </script>
