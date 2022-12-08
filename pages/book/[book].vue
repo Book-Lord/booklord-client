ff4f54604b6c9b71efe2bd0fd720a9f9f87e110d
Iskren Alexandrov <iskren.alexandrov@gmail.com>
2022-12-08 09:01:33 +0200

    Add book reviews

--- pages/book/[book].vue
+++ pages/book/[book].vue
@@ -1,8 +1,29 @@
 <script setup>
+    const user = useSupabaseUser()
+    const client = useSupabaseAuthClient()
+
+    const userId = computed(() => user.value?.id)
     const apiBase = useRuntimeConfig().apiBase
     const route = useRoute()
 
-    const { data: bookData } = await useAsyncData('getInfo', () => $fetch(apiBase + `/book/${route.params.book}`) )
+    const { data: data } = await useAsyncData('getInfo', () => $fetch(apiBase + `/book/${route.params.book}`) )
+    const bookData = data.value.bookInfo
+    const reviews = data.value.reviews
+    
+    const reviewContent = ref('')
+    const sendReview = async () => {
+        const { data: newReview } = await $fetch(apiBase + `/reviews`, {
+            method: 'post',
+            body: {
+                bookId: route.params.book,
+                userId: userId.value,
+                rating: 3,
+                content: reviewContent.value
+            }
+        })
+        console.log(newReview)
+        reviewContent.value = ''
+    }
 </script>
 
 <template>
@@ -14,6 +35,26 @@
         </div>
         <div class="float-right m-5 mr-20 inline-block">
             <img :src="bookData.coverImg" alt="">
+        </div>
+        <div class="m-3 w-[40rem] inline-block">
+            <p class="bold text-3xl">Reviews</p>
+            <div class="mt-5">
+                <button @click="sendReview" class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded">Add Review</button>
+                <input v-model="reviewContent" class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" type="text" name="review" placeholder="Add a review" autocomplete="off" />
+            </div>
+            <div v-for="(review, idx) in reviews" :key="idx" >
+                <div class="mt-5">
+                    <div class="flex mt-5">
+                        <div class="w-1/4">
+                            <!-- <img class="rounded-full" :src="review.userImg" alt=""> -->
+                        </div>
+                        <div class="w-3/4">
+                            <p class="bold">{{ getAuthorEmail(review.userId) }}</p>
+                            <p class="text-sm">{{ review.content }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -6,6 +6,15 @@ definePageMeta({
 const user = useSupabaseUser()
 const client = useSupabaseAuthClient()
 const email = computed(() => user.value?.email)
+const userId = computed(() => user.value?.id)
+const apiBase = useRuntimeConfig().apiBase
+
+const uri = apiBase + `/users/${userId.value}`
+
+const { data: userInfo } = await $fetch(uri, { 
+        method: 'post',
+        body: { email: user.value?.email }
+})
 
 console.log(user.value)
 
@@ -22,6 +31,9 @@ onMounted(() => {
         <p class="mt-4">Welcome to your profile page</p>
         <p class="mt-4">Your email is:</p>
         <p>{{ email }}</p>
+        <p>{{ userId }}</p>
+        <br>
+        <p>{{ userInfo }}</p>
 
         <button @click="client.auth.signOut()" class="mt-20 p-2 font-medium bg-green-500 rounded hover:bg-green-400">Logout</button>
     </div>
