0e7f21adc5384e730d2f975b764cf6e42d7484dc
Iskren Aleksandrov <iskren.alexandrov@gmail.com>
2023-02-15 20:36:11 +0200

    Import everything from vue-toast

--- plugins/vue-toastificaton.client.js
+++ plugins/vue-toastificaton.client.js
@@ -1,6 +1,9 @@
 import { defineNuxtPlugin } from '#app'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
+import * as pkg from "vue-toastification"
+
+const { useToast } = pkg
 
 export default defineNuxtPlugin((nuxtApp) => {
     const options = {
