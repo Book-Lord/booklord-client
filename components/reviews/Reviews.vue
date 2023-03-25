6546ea762c563850e9dbd21c0dd2403afa7bd9f5
Iskren Aleksandrov <iskren.alexandrov@gmail.com>
2023-03-25 13:47:38 +0200

    Add modal for review creation

--- components/reviews/Reviews.vue
+++ components/reviews/Reviews.vue
@@ -2,14 +2,32 @@
     <div class="m-3 mt-10 inline-block">
         <p class="text-4xl font-thin">Reviews</p>
         <div class="mt-5">
-            <form @submit.prevent="sendReview">
-                <input v-model="reviewContent" class="border-2 border-gray-300 bg-white h-10 mx-4 pr-16 rounded-lg text-sm focus:outline-none" type="text" name="review" placeholder="Add a review" autocomplete="off" />
-                <div class="inline-block">
-                    <label for="medium-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rating ({{ (rating / 10).toFixed(0) }})</label>
-                    <input v-model="rating" id="medium-range" type="range" class="h-2 mr-6 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+            <button @click="showModal = !showModal" class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-8 rounded">Create a review</button>
+            <modal v-show="showModal" @close-modal="showModal = false">
+                <div class="p-4">
+                    <form @submit.prevent="sendReview">
+                        <h2 class="text-lg font-semibold mb-4">Add a Review</h2>
+                        <div class="flex items-center mb-4">
+                            <label class="text-sm font-medium mr-2">Rate this book</label>
+                            <div class="w-full h-2 bg-gray-300 rounded-full relative">
+                                <div class="h-2 bg-purple-500 rounded-full" :style="{ width: `${rating * 10}%` }"></div>
+                                <input v-model="rating" type="range" min="0" max="10" class="absolute inset-0 opacity-0 cursor-pointer z-10 w-full h-full">
+                            </div>
+                            <span class="text-sm font-medium ml-2">{{ rating }}</span>
+                            <input v-model="rating" type="range" min="0" max="10" class="sr-only">
+                        </div>
+                        <div class="mb-4">
+                            <label class="text-sm font-medium mb-2">Leave a review below:</label>
+                            <textarea placeholder="I think..." v-model="reviewContent" class="w-full h-36 border-2 border-gray-300 bg-white px-4 py-2 rounded-lg text-sm focus:outline-none"></textarea>
+                        </div>
+                        <button class="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-8 rounded-full self-center">Send</button>
+                    </form>
                 </div>
-                <button class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-8 rounded">Add Review</button>
-            </form>
+                    <!-- <div>
+                        <label for="medium-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rating ({{ (rating / 10).toFixed(0) }})</label>
+                        <input v-model="rating" id="medium-range" type="range" class="h-2 mr-6 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+                    </div>                -->
+            </modal>
         </div>
         <div class="rounded bg-gray-50 mt-3 max-w-4xl max-h-screen overflow-y-scroll">
             <div class="sticky top-0 p-3 bg-gray-50 rounded">
@@ -44,6 +62,8 @@
 
     const reviewContent = ref('')
     const rating = ref(0)
+
+    const showModal = ref(false)
     
     const sendReview = async () => {
         if (!userId.value) {
@@ -65,8 +85,7 @@
             method: 'post',
             headers: { 'Authorization': `Bearer ${useSupabaseToken().value}` },
             body: {
-                // Transform to 1-10 scale
-                rating: (rating.value / 10).toFixed(0),
+                rating: rating.value,
                 content: reviewContent.value
             }
         }).then( (res) => {
@@ -80,6 +99,8 @@
             console.error(err);
 
             toast.error('Failed to add review 😢')
-        })  
+        }).finally( () => {
+            showModal.value = false
+        })
     }
 </script>
